f16e8b5544164a316c7ec5f7aa456afc937e1f76
clebron949 <christopher.lebron1@gmail.com>
2025-06-20 08:25:04 -0400

    feature - add url field

--- PasswordManager/PasswordStore/PasswordStoreRepository.cs
+++ PasswordManager/PasswordStore/PasswordStoreRepository.cs
@@ -3,12 +3,22 @@ using MySqlConnector;
 
 namespace PasswordManager.PasswordStore;
 
+public class PasswordStoreModel
+{
+    public int Id { get; init; }
+    public string Name { get; set; } = "";
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string Url { get; set; } = "";
+    public DateTime CreatedDate { get; init; }
+}
+
 public class PasswordStoreRepository(string connectionString)
 {
 
     public async Task<IEnumerable<PasswordStoreModel>> GetAll()
     {
-        await using var connection = new MySqlConnection(connectionString); 
+        await using var connection = new MySqlConnection(connectionString);
         var results = await connection.QueryAsync<PasswordStoreModel>("SELECT * FROM passwordStore");
         return results ?? [];
     }
@@ -26,34 +36,26 @@ public class PasswordStoreRepository(string connectionString)
             return null;
         }
     }
-    
+
     public async Task<int> Add(PasswordStoreModel model)
     {
         await using var connection = new MySqlConnection(connectionString);
-        var result = await connection.ExecuteAsync(@"INSERT INTO passwordStore (Name, Username, Password) VALUES (@Name, @Username, @Password)", model);
+        var result = await connection.ExecuteAsync(@"INSERT INTO passwordStore (Name, Username, Password, Url) VALUES (@Name, @Username, @Password, @Url)", model);
         return result;
     }
-    
+
     public async Task<int> Update(PasswordStoreModel model)
     {
         await using var connection = new MySqlConnection(connectionString);
-        var result = await connection.ExecuteAsync(@"UPDATE passwordStore SET Name = @Name, Username = @Username, Password = @Password WHERE Id = @Id", model);
+        var result = await connection.ExecuteAsync(@"UPDATE passwordStore SET Name = @Name, Username = @Username, Password = @Password, Url = @Url WHERE Id = @Id", model);
         return result;
     }
-    
+
     public async Task Delete(int id)
     {
         await using var connection = new MySqlConnection(connectionString);
         await connection.ExecuteAsync(@"DELETE FROM passwordStore WHERE Id = @Id", new { Id = id });
     }
-    
+
 }
 
-public class PasswordStoreModel
-{
-    public int Id { get; init; }
-    public string Name { get; set; } = "";
-    public string Username { get; set; } = "";
-    public string Password { get; set; } = "";
-    public DateTime CreatedDate { get; init; }
-}
